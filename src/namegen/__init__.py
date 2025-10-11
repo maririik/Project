1c4e01bf1937ac79b779c085024bb974d50f0595
maririik <aurora.riikonen@gmail.com>
2025-10-11 20:02:34 +0300

    Update __init__.py

--- src/namegen/__init__.py
+++ src/namegen/__init__.py
@@ -1,5 +1,3 @@
-
-
 from .trie import NGramTrie
-from .namegen import NGramGenerator, sample_weighted
+from .generator import NGramGenerator, sample_weighted
 __all__ = ["NGramTrie", "NGramGenerator", "sample_weighted"]
